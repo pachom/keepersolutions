aad5ec3b9d18490865db8600ff9263781a0f0932
Francisco <pachom@gmail.com>
2021-10-20 11:20:55 -0500

    create Bookmark Model and make migration

--- bookmarks/models.py
+++ bookmarks/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.db.models.fields import CharField
 
 # Create your models here.
+class Bookmark(models.Model):
+    title = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, unique=True)
+
+    url = models.URLField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('title',)
+    
+    def __str__(self):
+        return self.title
